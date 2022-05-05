a48667a4eca491c86b0175851ee37ce3e2b64504
citadel <unitedmilitaryum@gmail.com>
2022-05-04 17:45:44 -0700

    pushing updates

--- cogs/commands.py
+++ cogs/commands.py
@@ -3,6 +3,10 @@ import discord
 from . import utils
 from discord import Color
 from datetime import datetime
+import re
+import json
+import urllib.request
+
 
 class Commands(commands.Cog):
     def __init__(self, bot):
@@ -11,12 +15,12 @@ class Commands(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         await utils.embedDetails(ctx,
-        "WockyFX Help Menu", 
-        "```====== User Commands ======\n.help: Displays this menu\n.info: Displays general info\n.server_info: Displays information on the server\n.userinfo: Displays info on a mentioned user\n.gp: Ghost Ping a mentioned role a specific amount of times LOL```", 
-        Color.gold(), 
-        ctx.guild.icon_url, 
-        datetime.utcnow(), 
-        "WockyFX - Help Menu")
+                                 "WockyFX Help Menu",
+                                 "```====== User Commands ======\n.help: Displays this menu\n.info: Displays general info\n.server_info: Displays information on the server\n.userinfo: Displays info on a mentioned user\n.gp: Ghost Ping a mentioned role a specific amount of times LOL```",
+                                 Color.gold(),
+                                 ctx.guild.icon_url,
+                                 datetime.utcnow(),
+                                 "WockyFX - Help Menu")
 
     @commands.command()
     async def info(self):
@@ -25,12 +29,13 @@ class Commands(commands.Cog):
     @commands.command()
     async def server_info(self, ctx):
         await utils.embedDetails(ctx,
-        "Server Info for: {}".format(ctx.guild.name),
-        "```Server Name: {}\nServer Owner: {}\nServer ID: {}\nServer Icon Url: {}```".format(ctx.guild.name, ctx.guild.owner.name, ctx.guild.id, ctx.guild.icon_url),
-        Color.gold(), 
-        ctx.guild.icon_url, 
-        datetime.utcnow(),
-        "WockyFX - Server Info")
+                                 "Server Info for: {}".format(ctx.guild.name),
+                                 "```Server Name: {}\nServer Owner: {}\nServer ID: {}\nServer Icon Url: {}```".format(
+                                     ctx.guild.name, ctx.guild.owner.name, ctx.guild.id, ctx.guild.icon_url),
+                                 Color.gold(),
+                                 ctx.guild.icon_url,
+                                 datetime.utcnow(),
+                                 "WockyFX - Server Info")
 
     @commands.command()
     async def user_info(self):
@@ -46,8 +51,33 @@ class Commands(commands.Cog):
 
     @commands.command()
     async def membercount(self, ctx):
-        await utils.embedDetails(ctx, "Total Member Count", "Total Members: {}".format(ctx.guild.member_count), Color.gold(), ctx.guild.icon_url, datetime.utcnow(), "WockyFX - Member Count")
+        await utils.embedDetails(ctx, "Total Member Count", "Total Members: {}".format(ctx.guild.member_count),
+                                 Color.gold(), ctx.guild.icon_url, datetime.utcnow(), "WockyFX - Member Count")
 
+    @commands.command()
+    async def ipgeo(self, ctx, host):
+        await ctx.message.delete()
+        url = 'http://ipinfo.io/{}?token=7c22b876dc7676'.format(host)
+        response = urllib.request.urlopen(url)
+        data = json.load(response)
+
+        host = data['ip']
+        hostname = data['hostname']
+        loc = data['loc']
+        postal = data['postal']
+        timezone = data['timezone']
+        org = data['org']
+        city = data['city']
+        country = data['country']
+
+        await utils.embedDetails(ctx,
+                                 "IP Info on: {}".format(host),
+                                 "IP: {}\nHostname: {}\nLocation: {}\nCity: {}\nCountry: {}\nPostal Code: {}\nTimezone: {}\nOrg: {}".format(
+                                     host, hostname, loc, city, country, postal, timezone, org),
+                                 Color.gold(),
+                                 ctx.guild.icon_url,
+                                 datetime.utcnow(),
+                                 "WockyFX - IP Geo", 0)
 
 
 def setup(bot):
--- cogs/db/users.py
+++ cogs/db/users.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 import discord
+from discord import Color
+from datetime import datetime
 from .. import utils
 import sqlite3
 import hashlib
@@ -12,6 +14,7 @@ class Users(commands.Cog):
     @commands.command()
     @commands.has_any_role("CEO", "Co-Owner", "•")
     async def add_db_user(self, ctx, username, password, email, id):
+        await ctx.message.delete()
         hashed_password = hashlib.sha1(password.encode())
         pass_value = hashed_password.hexdigest()
         task = "INSERT INTO DB_USERS (USERNAME, PASSWORD, EMAIL, USERID) VALUES (?,?,?,?)"
@@ -19,13 +22,34 @@ class Users(commands.Cog):
         conn.execute(task, (str(username), str(pass_value), str(email), str(id)))
 
         conn.commit()
-        await ctx.send("User added to DB!")
+        await utils.embedDetails(ctx,
+                                 "Database Changed!",
+                                 "{} has been added to the database by: {}".format(username, ctx.author.mention),
+                                 Color.gold(),
+                                 ctx.guild.icon_url,
+                                 datetime.utcnow(),
+                                 "WockyFX - Database Changed", 0)
         conn.close()
 
     @commands.command()
     @commands.has_any_role("CEO", "Co-Owner", "•")
-    async def delete_db_user(self, ctx, username, password, email, member: discord.Member = None):
-        pass
+    async def delete_db_user(self, ctx, username, password, email, id):
+        await ctx.message.delete()
+        hashed_password = hashlib.sha1(password.encode())
+        pass_value = hashed_password.hexdigest()
+        task = "DELETE FROM DB_USERS WHERE USERNAME = ? AND PASSWORD = ? AND EMAIL = ? AND USERID = ?"
+        conn = sqlite3.connect("db/users.db")
+        conn.execute(task, (str(username), str(pass_value), str(email), str(id)))
+
+        conn.commit()
+        await utils.embedDetails(ctx,
+                                 "Database Changed!",
+                                 "{} has been removed from the database by: {}".format(username, ctx.author.mention),
+                                 Color.gold(),
+                                 ctx.guild.icon_url,
+                                 datetime.utcnow(),
+                                 "WockyFX - Database Changed", 0)
+        conn.close()
 
     @commands.command()
     @commands.has_any_role("CEO", "Co-Owner", "•")
